561b56d19a51a211d4f6263ff08f469b8c1aa642
newvid <thtjwls@newvid.co.kr>
2017-03-23 18:15:34 +0900

    커밋 테스트

---
+++ test.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<meta name="viewport" content="width=device-width"/>
+</head>
+<body>
+	<?
+		echo '<br> agent : ' . $_SERVER['HTTP_USER_AGENT'];
+	?>
+</body>
+</html>
