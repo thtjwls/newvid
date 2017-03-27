c0f731d7ae0612a8b6d2d75d12d64f466f2f805b
newvid <newvid@newvid.co.kr>
2017-03-27 14:16:10 +0900

    2017-03-27 jihoon

--- application/views/home/ci_v.php
+++ application/views/home/ci_v.php
@@ -1,1 +1,50 @@
-ci page
+<div class="ci_page">
+	<div class="row">
+		<!-- ci page title -->
+		<p>
+			<strong>새로운·도전적인·여유</strong>
+			뉴비드는 항상 새로운 것에 두려워 하지 않는 여유를 가지고  나아가자 나아가자 나아가자 나아가 시도하고 앞으로 나아 갑니다. 뉴비드는 항상 새로운 것에 두려워 하지 않는 여유를 가지고 합니다. 가나다라  NEW Vision Identity Director
+		</p>
+	</div>
+	<div class="row clearfix">
+		<!-- 기본형  -->
+		<h4 class="box-title">
+			기본형
+		</h4>
+		<div class="logo_left">
+			<!-- float left -->
+			<img src="/images/ci_img/logo_ci_main.png" alt="">
+			<p class="color-guide">Color Guide</p>
+			<ul>
+				<li><span class="color_boolet" style="background-color:#E8382F;"></span>C:0 M:90 Y:80 K:0</li>
+				<li><span class="color_boolet" style="background-color:#E8382F;"></span>C:0 M:75 Y:50 K:0</li>
+				<li><span class="color_boolet" style="background-color:#E8382F;"></span>C:0 M:0 Y:0 K:0</li>
+			</ul>
+		</div>
+		<div class="logo_right">
+			<!-- float right -->
+			<img src="/images/ci_img/logo_ci_main_black.png" alt="">
+			<p class="color-guide">Color Guide</p>
+			<ul>
+				<li><span class="color_boolet" style="background-color:#E8382F;"></span>C:0 M:0 Y:0 K:80</li>
+				<li><span class="color_boolet" style="background-color:#E8382F;"></span>C:0 M:0 Y:0 K:60</li>
+				<li><span class="color_boolet" style="background-color:#E8382F;"></span>C:0 M:0 Y:0 K:40</li>
+			</ul>
+		</div>
+	</div>
+	<div class="row clearfix">
+		<!-- 응용형 -->
+		<h4 class="box-title">
+			응용형
+		</h4>
+		<div class="logo_list">
+			<!-- float -->
+			<img src="/images/ci_img/logo_ci_subbox_nomalred.png" alt="">
+			<img src="/images/ci_img/logo_ci_subbox.png" alt="">
+			<img src="/images/ci_img/logo_ci_subbox_blue.png" alt="">
+			<img src="/images/ci_img/logo_ci_subbox_nomal.png" alt="">
+			<img src="/images/ci_img/logo_ci_subbox_gray1.png" alt="">
+			<img src="/images/ci_img/logo_ci_subbox_gray2.png" alt="">
+		</div>
+	</div>
+</div>
--- application/views/home/environment_v.php
+++ application/views/home/environment_v.php
@@ -1,1 +1,4 @@
-environment_v page
+<pre>
+Web 의뢰
+기본정보 수집( 색상, 폰트, 이미지, 사이트맵 등 ) - 1차 시안 (이미지) - html,css 작업 - 
+</pre>
